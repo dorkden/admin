914b1c3f3db0709340d54b0d0038341515d19d1d
Dork Den <dorkden@hotmail.com>
2023-01-25 08:21:45 +0700

    remove filled login form

--- app/Filament/Pages/Auth/Login.php
+++ app/Filament/Pages/Auth/Login.php
@@ -6,14 +6,4 @@ use Filament\Http\Livewire\Auth\Login as BasePage;
 
 class Login extends BasePage
 {
-    public function mount(): void
-    {
-        parent::mount();
-
-        $this->form->fill([
-            'email' => 'admin@filamentphp.com',
-            'password' => 'password',
-            'remember' => true,
-        ]);
-    }
 }
